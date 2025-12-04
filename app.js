a2b52da751812507fc0164068a67a3d301f73892
younes BENNACEUR <younesbennaceur06@gmail.com>
2025-12-04 05:13:20 +0100

    backend push notif

---
+++ app.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import cron from 'node-cron';
+
+
+import connectDB from './lib/db.js'; 
+import reminderRoutes from './routes/reminderRoute.js';
+import { sendDailyPrayers } from './controllers/pushController.js';
+
+const app = express();
+
+connectDB();
+
+app.use(cors()); 
+app.use(express.json()); 
+
+app.use('/api', reminderRoutes);
+
+cron.schedule('54 4 * * *', () => {
+  console.log("⏰ DRIIIIING ! Rappel envoyé à 3h38");
+  sendDailyPrayers();
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur lancé sur le port ${PORT}`);
+});
